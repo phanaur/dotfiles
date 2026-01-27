dcde907cf222ba07a24694e0bf23049bb074391b
phanaur <davidominguezroman@gmail.com>
2026-01-27 22:35:08 +0100

    Configure cursor shapes for Neovim modes
    - Normal/Command mode: block █
    - Insert mode: vertical bar (25% width) ┃
    - Visual mode: underline (20% height) ▁
    - Replace mode: underline (20% height) ▁
    
    Consistent with Helix cursor configuration for better mode visibility

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Cursor shape configuration for different modes
+vim.opt.guicursor = {
+  "n-c:block",           -- Normal, command: bloque █
+  "i-ci-ve:ver25",       -- Insert: línea vertical (25% ancho) ┃
+  "v:hor20",             -- Visual: subrayado (20% altura) ▁
+  "r-cr:hor20",          -- Replace: subrayado (20% altura) ▁
+  "o:hor50",             -- Operator: subrayado (50% altura)
+  "a:blinkwait700-blinkoff400-blinkon250", -- Blinking settings
+  "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch
+}
